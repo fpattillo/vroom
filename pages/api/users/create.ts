700e28de9d5f153cc041d39043e8e244e74876a2
Benja Lepe <balepe@uc.cl>
2024-05-10 17:45:24 -0400

    Add hashed password

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -1,14 +1,30 @@
-
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+import axios from "axios";
+
+const ZAPPIER_EMAIL_GENERATOR_URL = "https://hooks.zapier.com/hooks/catch/7512145/3jf74p5/"
+
+function generateRandomPassword(length: number): string {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+// Función para encriptar la contraseña
+async function encryptPassword(password: string): Promise<string> {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  
   try {
-    const {company, email, amount, pandadoc,  currency, name} = req.body;
+    const { company, email, amount, pandadoc, currency, name } = req.body;
     if (!company || !email || !amount || !pandadoc || !name) {
-      throw new Error('Company, email, amount, pandadoc are required');
+      throw new Error("Company, email, amount, pandadoc are required");
     }
+    const password = generateRandomPassword(16);
+    const hashedPassword = await encryptPassword(password);
     await prisma.user.create({
       data: {
         name,
@@ -16,11 +32,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         email,
         amount: parseFloat(amount),
         pandadoc,
-        role: 'CLIENT',
+        role: "CLIENT",
         currency,
-      }
-    })
-    res.json({ message: 'Hello' })
+        password: hashedPassword,
+      },
+    });
+    axios.post(ZAPPIER_EMAIL_GENERATOR_URL, {
+      company,
+      email,
+      password,
+      name,
+    });
+    res.json({ message: "Hello" });
   } catch (e) {
     res.status(400).json({ error: (e as Error).message });
   }
