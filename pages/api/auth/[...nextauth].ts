ce3915b0d9f38c82b7f52f7fde52d13e3c204d58
Felipe Pattillo <fpattillo@uc.cl>
2024-05-10 17:27:52 -0400

    add password + bcrypt

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,9 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '../../../lib/prisma'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const bcrypt = require('bcrypt')
 
 /*
 * As we do not have backend server, the refresh token feature has not been incorporated into the template.
@@ -14,6 +17,30 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Correo electrónico", type: "text" },
+        password: { label: "Contraseña", type: "password" }
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.username },
+        })
+    
+        if (user && await bcrypt.compare(credentials.password, user.password)) {
+          return user
+        } else {
+          // If you return null, it will trigger an error on the client side "Credentials Sign In"
+          return null
+        }
+      }
     })
   ],
 });
--- pages/exercises.tsx
+++
@@ -1,9 +0,0 @@
-import ExerciseList from '../components/Exercises/exerciseList';
-
-export default function Exercises() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-white text-black font-sans">
-      <ExerciseList />
-    </main>
-  )
-}
